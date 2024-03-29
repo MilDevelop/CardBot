f70f56f09d27cced0939bfd1e8406d1dbe7177e0
MilDev <sychevegor0@gmail.com>
2024-03-29 22:33:23 +0700

    Snapshot_1 - 06

--- bot_code/Main_file.py
+++ bot_code/Main_file.py
@@ -1,4 +1,5 @@
 import telebot, keyboard_bot
+from random import randint
 from telebot import types
 from Bot_idetification import load_config
 from Additional_Resources.lexicon import LEXICON_RU
@@ -25,6 +26,12 @@ def on_click_start(message):
         bot.send_message(message.chat.id, 'Окей', reply_markup=types.ReplyKeyboardRemove()) #Вмнесто окей поставить смайлы мастей (периодично меняющихся)
         bot.send_message(message.chat.id, 'Происходит перетасовка карт...'
                                           'Сейчас вы увидете свои карты', Game_Start(), reply_markup=keyboard_bot.Player_field(Player.User_deck))
+        rand = randint(0, 1)
+        if rand == 0: #Переделать для дальнейших игр
+            bot.send_message(message.chat.id, LEXICON_RU['step_bot'])
+            bot.send_photo(message.chat.id, Bot_Game.Atack_Bot())
+        else:
+            bot.send_message(message.chat.id, LEXICON_RU['step_yourself'])
     elif message.text == LEXICON_RU['no_button']:
         bot.send_message(message.chat.id, 'Зачем вы тогда открыли игру...')
         bot.send_message(message.chat.id, 'Окей', reply_markup=types.ReplyKeyboardRemove())
@@ -37,9 +44,19 @@ def Game_Start():
     Bot_Game.GiveCards(deck.GiveAway_Card(Bot_Game.need_cards))
     Player.GiveCards(deck.GiveAway_Card(Player.need_cards))
 
+
+#bot.set_my_commands() -> after truble
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id, LEXICON_RU['/help'])
+
+@bot.message_handler(commands=['broken'])
+def broken(message):
+    bot.send_message(message.chat.id, LEXICON_RU['/broken'])
+
+@bot.message_handler(commands=['take'])
+def photo(message):
+    bot.send_message(message.chat.id, LEXICON_RU['/take'])
 
 @bot.message_handler(content_types=['text'])
 def other_text(message):
--- card_code/BOT_ACTIONS.py
+++ card_code/BOT_ACTIONS.py
@@ -1,3 +1,4 @@
+from Additional_Resources.Ieraration import Hierarchy
 class Bot_Game:
     def __init__(self):
         self.Bot_deck: list = []
@@ -9,4 +10,13 @@ class Bot_Game:
         else:
             for i in range(self.need_cards):
                 self.Bot_deck.append(main_deck[i])
+
+    def Atack_Bot(self):
+        rang_card: dict = {}
+        for element in self.Bot_deck:
+            if element in Hierarchy:
+                print(Hierarchy.index(element))
+                rang_card[Hierarchy.index(element)] = element
+        self.Bot_deck.pop(rang_card[min(list(rang_card.keys()))])
+        return rang_card[min(list(rang_card.keys()))]['images']['png']
 
