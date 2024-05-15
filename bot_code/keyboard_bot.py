82f8c0b6bd977c64ed285fb8610e682788fc98d9
Milimit <149592908+MilDevelop@users.noreply.github.com>
2024-05-15 14:05:19 +0700

    Update keyboard_bot.py
    reboot4

--- bot_code/keyboard_bot.py
+++ bot_code/keyboard_bot.py
@@ -1,5 +1,6 @@
 from telebot import types
-from Additional_Resources.lexicon import LEXICON_RU, simbol
+from Additional_Resources.lexicon import LEXICON_RU
+from bot_code.GAME import Game
 def first_keyboard():
     markup = types.ReplyKeyboardMarkup()
     markup_yes = types.KeyboardButton(LEXICON_RU['yes_button'])
@@ -7,9 +8,13 @@ def first_keyboard():
     markup.row(markup_yes, markup_no)
     return markup
 
-def Player_field(mass_card: list):
-    markup = types.ReplyKeyboardMarkup()
-    for i in range(len(mass_card)):
-        mark = types.KeyboardButton(f"{mass_card[i][0:len(mass_card[i])-3]}-{mass_card[i][-2:]}") #фиксить твот тут примерно
-        markup.row(mark)
-    return markup
+def Player_field(mass_card: list, game: Game):
+    if len(mass_card) > 1 and  game.complete[0] == False:
+        markup = types.ReplyKeyboardMarkup()
+        for i in range(len(mass_card)):
+            mark = types.KeyboardButton(f"{mass_card[i][0:len(mass_card[i])-3]}-{mass_card[i][-2:]}") #фиксить твот тут примерно
+            markup.row(mark)
+        return markup
+    else:
+        markup = types.ReplyKeyboardRemove()
+        return markup
