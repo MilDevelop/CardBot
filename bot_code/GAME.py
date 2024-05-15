41825ca787cf93319894f89673e9eb528e93b388
Milimit <149592908+MilDevelop@users.noreply.github.com>
2024-05-15 22:22:34 +0700

    Update GAME.py

--- bot_code/GAME.py
+++ bot_code/GAME.py
@@ -1,22 +1,11 @@
-import re
 class Game:
     def __init__(self):
         self.filter: bool = False
         self.complete: list = [False, "none"]
         self.score: int = 0
-    def add_score(self, id):
-        with open('Players.txt') as f:
-            lines = f.readlines()
-        str = id
-        pattern = re.compile(re.escape(str))
-        with open('Players.txt', 'w') as f:
-            for line in lines:
-                result = pattern.search(line)
-                if result is None:
-                    f.write(line)
-
-
+    def add_score(self):
+        self.score += 10
 
     def add_player(self, id, name):
         with open("Players.txt", "w") as myfile:
-            myfile.write(f"{name} - {id} - {self.score}\n")
+            myfile.write(f"{name} - {id}\n")
